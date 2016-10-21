e044f05557871e844a91eb852cfc4397e10c917f
07max1999 <ankit.baing.ab@gmail.com>
2016-10-21 15:41:51 +0530

    Concatenate list
    Add two list in python

---
+++ concatenate_4615.py.py
@@ -0,0 +1,15 @@
+a=[]
+b=[]
+AB=[]
+x=0
+while x<4:
+    x=int(input('enter no. : '))
+    a.append(x)
+    y=int(input('enter no. :  '))
+    b.append(y)
+print('List1 : ',a)
+
+print('list2 : ',b)
+AB=a+b
+print('Concatenate list1 & list2 :  ',AB)
+    
